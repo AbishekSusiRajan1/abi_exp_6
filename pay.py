6ea9e2a28a5a4e27ca2d37567beccbb1f23196bd
Ashikpaul07 <ashikpaul@karunya.edu.in>
2025-04-02 10:20:10 +0530

    fix #1 adding comments

--- pay.py
+++ pay.py
@@ -1,3 +1,4 @@
+#creating class employee
 class Employee:
     def __init__(self, emp_id, name, basic_salary, tax_rate, bonus=0):
         self.emp_id = emp_id
@@ -5,12 +6,12 @@ class Employee:
         self.basic_salary = basic_salary
         self.tax_rate = tax_rate
         self.bonus = bonus
-
+#function to calculate net salary
     def calculate_net_salary(self):
         tax_deduction = self.basic_salary * self.tax_rate
         net_salary = self.basic_salary - tax_deduction + self.bonus
         return net_salary
-
+#function to generate payslip
     def generate_payslip(self):
         payslip = f"""
         ============================
@@ -24,6 +25,6 @@ class Employee:
         """
         return payslip
 
-# Example Usage
+#initilizing value
 employee1 = Employee(emp_id="E101", name="Abishek", basic_salary=50000, tax_rate=0.1, bonus=5000)
 print(employee1.generate_payslip())
